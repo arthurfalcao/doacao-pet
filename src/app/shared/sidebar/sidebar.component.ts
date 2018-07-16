5285de33b110369230ead5910aee8a0bc0dc5f45
Arthur Falcão <arthurfalcao77@gmail.com>
2018-07-16 01:14:16 -0300

    pag pets

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  public isCollapsed = false;
 
   constructor() { }
 
