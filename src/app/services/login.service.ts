480532300f30d6ed5567e1c72391b2700912e09c
franklinmelo <franklinmelo18@gmail.com>
2018-07-18 15:07:16 -0300

    adjust

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from '../../../node_modules/rxjs';
 import { AngularFireAuth } from '../../../node_modules/angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { Router } from '../../../node_modules/@angular/router';
+import { Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +11,22 @@ import * as firebase from 'firebase/app';
 export class LoginService {
 
   user: Observable<firebase.User>;
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, private location: Location) {
     this.user = afAuth.authState;
    }
 
    loginGoogle(){
      const provider = new firebase.auth.GoogleAuthProvider();
-     this.afAuth.auth.signInWithPopup(provider);
+     this.afAuth.auth.signInWithPopup(provider).then(ok =>{
+       this.location.back();
+     });
    }
 
    loginFacebook(){
      const provider = new firebase.auth.FacebookAuthProvider();
-     this.afAuth.auth.signInWithPopup(provider);
+     this.afAuth.auth.signInWithPopup(provider).then(ok =>{
+      this.location.back();
+    });
    }
 
    logout(){
